f5a936a9a4f0e73c65df828ae795eb51da18012c
maioy97 <mai.algendyfcis@gmail.com>
2019-10-19 14:42:17 +0200

    first attempt using haar cascade

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # IP-practice
-a few image processing functions redone for practice 
+a few image processing functions redone for practice
+
+cascade document : https://docs.opencv.org/3.4/db/d28/tutorial_cascade_classifier.html
+
+alternate for the facial feature detection :
+    installing dlib : https://www.pyimagesearch.com/2017/03/27/how-to-install-dlib/
---
+++ featuredetection.py
@@ -0,0 +1,47 @@
+from __future__ import print_function
+import cv2 as cv
+import argparse
+
+def detectAndDisplay(frame):
+    frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    frame_gray = cv.equalizeHist(frame_gray)
+    #-- Detect faces
+    faces = face_cascade.detectMultiScale(frame_gray)
+    for (x,y,w,h) in faces:
+        center = (x + w//2, y + h//2)
+        frame = cv.ellipse(frame, center, (w//2, h//2), 0, 0, 360, (255, 0, 255), 4)
+        faceROI = frame_gray[y:y+h,x:x+w]
+        #-- In each face, detect eyes
+        eyes = eyes_cascade.detectMultiScale(faceROI)
+        for (x2,y2,w2,h2) in eyes:
+            eye_center = (x + x2 + w2//2, y + y2 + h2//2)
+            radius = int(round((w2 + h2)*0.25))
+            frame = cv.circle(frame, eye_center, radius, (255, 0, 0 ), 4)
+    cv.imshow('Capture - Face detection', frame)
+
+parser = argparse.ArgumentParser(description='Code for Cascade Classifier tutorial.')
+parser.add_argument('--face_cascade', help='Path to face cascade.', default='haar_xmls/haarcascade_frontalface_alt.xml')
+parser.add_argument('--eyes_cascade', help='Path to eyes cascade.', default='haar_xmls/haarcascade_eye_tree_eyeglasses.xml')
+parser.add_argument('--image', help='Image path.', default="image-1.jpeg")
+args = parser.parse_args()
+face_cascade_name = args.face_cascade
+eyes_cascade_name = args.eyes_cascade
+face_cascade = cv.CascadeClassifier()
+eyes_cascade = cv.CascadeClassifier()
+
+#-- 1. Load the cascades
+if not face_cascade.load(cv.samples.findFile(face_cascade_name)):
+    print('--(!)Error loading face cascade')
+    exit(0)
+if not eyes_cascade.load(cv.samples.findFile(eyes_cascade_name)):
+    print('--(!)Error loading eyes cascade')
+    exit(0)
+
+#-- 2. Read the video stream
+image = cv.imread(args.image)
+if  image is  None:
+    print('--(!)Error opening image')
+    exit(0)
+else:
+    detectAndDisplay(image)
+    cv.waitKey(0)
