de567f71bb4be7efbb5a702287b51783893f571f
maioy97 <mai.algendyfcis@gmail.com>
2019-10-21 17:37:03 +0200

    sobel in grayscale is done

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ a few image processing functions redone for practice
 to use the face detection script run in console in an enviroment that has opencv and mtcnn installed
 follow the MTCNN installation in : https://github.com/ipazc/mtcnn/
 
+to use the c++ file simply update the image list to the images you have and selsect them in run time the image list is 0 based 
--- filterfunctions.cpp
+++ filterfunctions.cpp
@@ -13,20 +13,31 @@ using namespace cv;
 
 //extracts a 3*3 window from the image 
 void fill_window(int** window,int pixelx,int pixely,Mat image,int channel){
-	Size imsize = image.size();
-	int img_width = imsize.width;
-	int img_height = imsize.height;
-	 
+
+	int img_width = image.cols;
+	int img_height = image.rows;
+	int temp;
+	//cout << "x" << pixelx << "y" << pixely << endl;
 	for ( int i = 0; i < 3; i++) {
 		//the one before
+		
 		for (int j=0;j<3;j++){
-			int used_x = pixelx - 1 + i;
-			int used_y = pixely - 1 + j;
-			if(used_x > img_width||used_x < 0 || used_y > img_height||used_y < 0)
+			int used_x = pixelx - 1 + j;
+			int used_y = pixely - 1 + i;
+			
+			if(used_x >= img_width||used_x < 0 || used_y >= img_height||used_y < 0)
 				window[i][j] = 0;
 			else							// row , col , channel 
-				window[i][j] = image.at<uchar>(used_y,used_x); }
+			{
+ 				temp = image.at<uchar>(used_y, used_x);
+				window[i][j] = temp;
+				
+			}
+			//cout << "Ux" << used_x << "Uy" << used_y <<" " <<window[i][j] << " ";
+		}
+		//cout << endl;
 	}
+	//cout << endl;
 }
 
 //dot product of 2 arrays of width*height
@@ -41,83 +52,85 @@ int dot_product(int** window,int kernel[3][3]){
 }
 
 Mat sobel_filter(Mat image){
-	int kernel[3][3] = { {-1,0,1}
+	int kernel1[3][3] = { {-1,0,1}
 						,{-2,0,2} 
 						,{-1,0,1} };
-	
+	int kernel2[3][3] = { {1,2,1}
+						,{0,0,0}
+						,{-1,-2,-1} };
 	int img_width = image.cols;
 	int img_height = image.rows;
 
-	
 	int** window = new int* [3];
-	Mat result = image.clone();
+	Mat result1 = image.clone(),result2 = image.clone(),result;
 	
-
 	for (int i = 0; i < 3; i++)
 		window[i] = new int[3];
-	/*int** imagecopy = new int* [img_width];
-	for (int i = 0; i < img_height; i++) 
-		imagecopy[i] = new int[img_height];
-	
-	memcpy(imagecopy, image.data, img_width * img_height * sizeof(int));
-	*/
+	vector<Mat> bgr_channels;
+	split(image, bgr_channels);
+
+
 	//apply sobel filter  3*3 matrix multiplication
-	for (int imagey = 0; imagey < img_height; imagey++){
-        for (int imagex = 0; imagex < img_width; imagex++) {
-
+	for (int imagex = 0; imagex < img_width; imagex++) {
+		
+        for (int imagey = 0; imagey < img_height ; imagey++) {
+			//cout<<"loop top: row" << imagey <<"  colomn" <<imagex << endl;
 			fill_window(window, imagex, imagey, image,0);
-			if (imagey == img_height-1)
-				int x=0;
-			result.at<uchar>(imagey,imagex) = dot_product(window, kernel);
-
-			/*fill_window(window, imagex, imagey, image, 1);
-			result.at<uchar>(imagey, imagex, 1) = dot_product(window, kernel);
-
-			fill_window(window, imagex, imagey, image, 2);
-			result.at<uchar>(imagey, imagex, 2) = dot_product(window, kernel);*/
-        }
+			result1.at<uchar>(imagey,imagex) = dot_product(window, kernel1);
+			
+			fill_window(window, imagex, imagey, image, 0);
+			result2.at<uchar>(imagey, imagex) = dot_product(window, kernel2);
+			}
     }
-	//memcpy(result.data, imagecopy, img_width * img_height * sizeof(int));
-	//delete imagecopy;
-	delete window;
+	add(result1, result2, result);
+	delete [] window;
 	return result;
 }
 
 Mat orange_filter(int img_width,int img_height, Mat image){
 	 
-	int** imagecopy = new int* [img_width];
 	Mat result;
 
-	for (int i = 0; i < img_height; i++)
-		imagecopy[i] = new int[img_height];
+	
 	int r=0, g=0, b=0;
 	//fill rgb values 
-     for (int i = 0; i<img_width;i++){
-        for(int j =0 ;i<img_height;j++)
+     for (int x = 0; x<img_width;x++){
+        for(int y =0 ;y<img_height;y++)
         {	// orange has a range of 255, >50 , _
-			if (r == 255, g >= 50) 
+			Vec3f intensity = image.at<Vec3f>(y, x);
+			float blue = intensity.val[0];
+			float green = intensity.val[1];
+			float red = intensity.val[2];
+
+			if (red == 255, green >= 50)
 			{
-				r = 0, g = 0, b = 0;
+				red = 0, green = 0, blue = 0;
+				
 				//move those to the mat result 
+				result.at<Vec3f>(y, x)[0] = blue;
+				result.at<Vec3f>(y, x)[1] = green;
+				result.at<Vec3f>(y, x)[2] = red;
 			}
             
         }
     } 
-	 delete imagecopy;
-	 memcpy(result.data, imagecopy, img_width * img_height * sizeof(int));
+	 //memcpy(result.data, imagecopy, img_width * img_height * sizeof(int));
 	 return result;
 }
 
 int main (){
 
-  int choice,int_width,int_height;
+  int choice_fltr,choice_img,int_width,int_height;
   Mat image;
-  string default_path;
+  string image_names[] = { "image-2","test","test2" };
+  string default_path = "D:\\Redirected Profile\\Documents\\projects\\git\\IPvisualstudio\\";
   cout<<"pick an operation:1 sobel filter, 2 orange color filter";
-  cin>>choice;
-  //cout<<"enter image ,0 to use default path:" + default_path;
+  cin>>choice_fltr;
+  cout<<"enter image number :" ;
+  cin >> choice_img;
+   
   //load image 
-  image = imread("D:\\Redirected Profile\\Documents\\projects\\git\\IPvisualstudio\\image-2.png");
+  image = imread(default_path+image_names[choice_img]+".png");
   
   
   Mat result;
@@ -125,14 +138,19 @@ int main (){
   int_width = image.cols;
   int_height = image.rows;
 
-  if (choice==1)
-      result = sobel_filter(image); 
+  if (choice_fltr == 1){
+	  image = imread(default_path + image_names[choice_img] + ".png",IMREAD_GRAYSCALE);
+	  result = sobel_filter(image);
+  }
 
-  else if (choice==2)
-      result = orange_filter(int_width, int_height,image); 
-
+  else if (choice_fltr==2){ 
+	  image = imread(default_path + image_names[choice_img] + ".png", IMREAD_GRAYSCALE); 
+	  result = orange_filter(int_width, int_height, image);
+  }
+      
+  imwrite(image_names[choice_img] + "out.png",result);
   imshow("result", result);
   waitKey(0);
- //display image
+
   return 0;
 }
